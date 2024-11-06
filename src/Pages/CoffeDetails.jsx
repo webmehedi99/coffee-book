d469ebcca1c61795846c25119d6c6cdff118e5e7
webmehedi99 <webmehedi99@gmail.com>
2024-11-07 04:05:50 +0600

    create coffee section and destructuring all thing what is needed for specific dynamic items

--- src/Pages/CoffeDetails.jsx
+++ src/Pages/CoffeDetails.jsx
@@ -11,9 +11,30 @@ const CoffeeDetails = () => {
         setCoffee(singleData);
     }, [allCoffeeData, id]);
 
+    const { name, image, ingredients, nutrition_info, description, making_process, rating, popularity } = coffee;
+
     return (
-        <div>
-            Coffee details: { coffee.description}
+        <div className='my-12'>
+            <h1 className='text-2xl md:text-4xl font-thin mb-8'>{description}</h1>
+
+            <div className='w-full h-full md:h-[500px]  object-cover overflow-hidden shadow-xl '>
+                <img className=' w-full h-full rounded-xl ' src={image} alt="" />
+            </div>
+
+            <div className='my-6 flex justify-between items-center'>
+                <div className='space-y-2'>
+                    <h1 className='text-2xl md:text-3xl font-thin'>{name}</h1>
+                    <p className='text-base'>Popularity : {popularity}</p>
+                    <p className='text-base'>Rating : {rating}</p>
+                </div>
+                <div>
+                    <button className='btn btn-warning text-base font-bold '>Add To Favorites</button>
+                </div>
+            </div>
+            <div className='my-6'>
+                  <h1 className='text-2xl md:text-3xl font-thin'>Making Process :</h1>
+                  <p className='text-base '>{making_process}</p>
+            </div>
         </div>
     );
 };
